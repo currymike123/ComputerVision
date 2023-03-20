e31afc0b6f27e77b29262e97d2c97f6c70410b8c
currymike <currymike@gmail.com>
2023-03-20 14:40:06 -0400

    Updates to Module2b and Final Project

---
+++ final_project_template.py
@@ -0,0 +1,96 @@
+##################################################################################
+# Libraries
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+##################################################################################
+# Variables to map the numeric values to the filter names.  We will use later.
+PREVIEW   = 0   # Preview Mode
+GRAY      = 1   # Grayscale Filter
+THRESHOLD = 2   # Threshold Filter
+
+##################################################################################
+# All your filter and tracker functions.
+def Threshold_filter(frame):
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    h = frame.shape[0]
+    w = frame.shape[1]
+
+    imgThres = np.zeros((frame.shape[0],frame.shape[1]), dtype=np.uint8)
+
+    for y in range(0,h):
+    #Search each pixel in the row
+        for x in range(0,w):
+        
+            #If the pixel value is above 100 set it to white.  If it is below 100 set it to black.
+            if (frame[y,x] > 100):
+                imgThres[y,x] = 255
+            else:
+                imgThres[y,x] = 0
+    
+    
+    return imgThres
+    
+##################################################################################
+# The video image loop.
+
+# Create a VideoCapture object and read from input file
+# If the input is taken from the camera, pass 0 instead of the video file name.
+cap = cv2.VideoCapture(0)
+
+# Variable to keep track of the current image filter. Default set to Preview.
+image_filter = PREVIEW
+
+# Video Loop
+while cap.isOpened():
+    # Ret = True if frame is read correctly, frame = the frame that was read.
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        break
+    
+    # Resize the image so your app runs faster.  Less Pixels to Process.  
+    # Use Nearest Neighbor Interpolation for the fastest runtime.
+    frame = cv2.resize(frame, None, fx = 0.3, fy = 0.3, interpolation=cv2.INTER_NEAREST)
+    
+    # Send your frame to each filter.
+    if image_filter == PREVIEW:
+        # No filter.
+        result = frame
+    elif image_filter == THRESHOLD:
+        # Send the frame to the Threshold function.
+        result = Threshold_filter(frame)
+    elif image_filter == GRAY:
+        # Convert to grayscale.
+        result = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Show your frame.  Remember you don't need to convert to RGB.  
+    cv2.imshow('frame', result)
+    
+    # Map all your key codes to your filters/trackers. Capital and lower case.
+    key = cv2.waitKey(1)
+
+    # Quit the app.
+    if key == ord('Q') or key == ord('q'):
+        break
+    # Grayscale filter
+    elif key == ord('G') or key == ord('g'):
+        image_filter = GRAY
+    # Threshold filter
+    elif key == ord('T') or key == ord('t'):
+        image_filter = THRESHOLD
+    # Preview. No filter.
+    elif key == ord('P') or key == ord('p'):
+        image_filter = PREVIEW
+
+##################################################################################
+# Close the app
+cap.release()
+cv2.destroyWindow('frame')
+
+
+
